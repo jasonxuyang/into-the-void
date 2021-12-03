d2de017760d440ead00559082c67541d78367043
jyang475 <jasonxuyang@gatech.edu>
2021-12-03 13:42:25 -0500

    add ending routing logic

---
+++ pages/bad-ending/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { useState } from 'react';
+import styles from './styles.module.scss'
+
+
+export default function Outro() {
+
+    return (
+        <main className={styles.outro_container}>
+            <p>This is the bad ending.</p>
+            <Link href='/app'>
+                <div className={`button`}>Play Again</div>
+            </Link>
+        </main>
+    )
+}
